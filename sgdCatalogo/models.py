ffb6e78fdba19a0eac98c80dc4c22452d33ccd46
diego <decoarmando@gmail.com>
2026-02-13 08:38:25 -0500

    cd

--- sgdCatalogo/models.py
+++ sgdCatalogo/models.py
@@ -94,7 +94,7 @@ class Referencia(models.Model):
     def __str__(self):
         return self.name
     
-class NivelSeguridad(models.Model):  #cambio de diego
+class NivelSeguridad(models.Model):  #cambio de diego tf
     name = models.CharField(max_length=100)
 
     class Meta:
